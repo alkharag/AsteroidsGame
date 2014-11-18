22eba7eab361d7c3de6d2cbdb3cffdd97777c2c9
alkharag <kharag.alex_sa@yahoo.com>
2014-11-18 10:25:22 -0800

    first try

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 //your variable declarations here
 private boolean keyUp = false;
 private boolean keyDown = false;
@@ -35,8 +51,8 @@ public void draw()
   player.show();
   player.move();
 
-  if(keyUp == true){player.accelerate(.1);}
-  if(keyDown == true){player.accelerate(-.1);}
+  if(keyUp == true){player.accelerate(.1f);}
+  if(keyDown == true){player.accelerate(-.1f);}
   if(keyRight == true){player.rotate(5);}
   if(keyLeft == true){player.rotate(-5);}
   
@@ -313,3 +329,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   } 
 } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
